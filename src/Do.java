836d49ab169470c20ade561f350a890fca3a799e
César Henrique Bernabé <cesar.hber@gmail.com>
2016-11-09 11:31:23 -0200

    Leitura de todos os arquivos da entrada

--- src/Do.java
+++ src/Do.java
@@ -1,12 +1,15 @@
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import trabfs.machineLeaningFrameWork.core.Problema;
 import trabfs.machineLeaningFrameWork.core.Result;
 import trabfs.machineLeaningFrameWork.search.Search;
 import trabfs.machineLeaningFrameWork.search.metaheuristics.GeneticSearch;
 import trabfs.machineLeaningFrameWork.search.metaheuristics.MeuMetodo;
-
 /**
  *
  * @author rbroetto
@@ -14,15 +17,27 @@ import trabfs.machineLeaningFrameWork.search.metaheuristics.MeuMetodo;
 public class Do {
     
     public static void main(String[] args){
-        
-        // instanciando o método de busca
-        Search metodo = new MeuMetodo();
+        File folder = new File("entrada");
+        File[] listOfFiles = folder.listFiles();
 
-        // instanciando o problema        
-        Problema p = new Problema("entrada/sonar.arff");
-        
-        // executando o método
-        Result r = metodo.startSearch(p);   
-        r.printResult();                
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                Search metodo = new MeuMetodo();
+                Problema p = new Problema("entrada/" + listOfFiles[i].getName());
+                Result r = metodo.startSearch(p); 
+                
+                r.printResult();
+            }
+        }
+    
+//        // instanciando o método de busca
+//        Search metodo = new MeuMetodo();
+//
+//        // instanciando o problema        
+//        Problema p = new Problema("entrada/sonar.arff");
+//        
+//        // executando o método
+//        Result r = metodo.startSearch(p);   
+//        r.printResult();                
     }    
 }
